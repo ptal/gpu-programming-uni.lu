fb3271d37cc2d1409d34b72fc05df700484901e1
Georgios Kafanas <georgios.kafanas@uni.lu>
2024-10-11 11:16:32 +0200

    Add benchmark timing function

--- 2-scan/utility.hpp
+++ 2-scan/utility.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <cstdio>
+#include <chrono>
 
 #pragma once
 
@@ -15,3 +16,20 @@ inline static void handle_error( cudaError_t err,
 }
 
 #define try_cuda( result ) handle_error( (result), __FILE__, __LINE__ )
+
+/** Benchmarks the time taken by the function `f` by executing it 1 time first
+ * for warm-up, then 10 times in a row, then dividing the result by 10.
+ * It returns the duration in milliseconds. */
+template<class F>
+double benchmark_averaged_ms(F&& f, unsigned long long int n) {
+  f(); // warm-up.
+
+  auto start = std::chrono::steady_clock::now();
+  for(int i = 0; i < n; ++i) {
+    f();
+  }
+  auto end = std::chrono::steady_clock::now();
+
+  std::chrono::milliseconds Dt = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  return static_cast<double>( Dt.count() /* This will be a unint64_t */ ) / static_cast<double>(n);
+}
