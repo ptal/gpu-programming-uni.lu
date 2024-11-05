5874c48093a1c7522599ac950fb515000c9f2960
Shahmeer-Khan99 <shahmeerskhan@gmail.com>
2024-11-05 13:19:47 +0100

    Added slurm scripts

---
+++ 3-scan/scan/slurm-run.sh
@@ -0,0 +1,8 @@
+srun --time=00:05:00 \
+  --nodes=1 \
+  --partition=gpu \
+  --cpus-per-task=1 \
+  --ntasks=1 \
+  --gpus-per-task=1 \
+  --export=ALL \
+  bash -l -c "module load compiler/NVHPC && ./cudaScan -m scan -n 40000000"
