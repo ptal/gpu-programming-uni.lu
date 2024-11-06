274e6223b76415479218b1775137e4c6925da39a
Shahmeer-Khan99 <shahmeerskhan@gmail.com>
2024-11-06 15:50:59 +0100

    Exclusive sum changed 9

--- 3-scan/scan/scan.cu
+++ 3-scan/scan/scan.cu
@@ -210,13 +210,13 @@ int find_repeats(int* device_input, int length, int* device_output) {
 
   // exclusive_scan(device_input, length, device_output);
   // cudaDeviceSynchronize();
-  int index = 0;
-  for(int i = 0; i < length; i++) {
-    if(device_input[i] == 1) {
-      device_output[index] = i;
-      index++;
-    }
-  }
+  // int index = 0;
+  // for(int i = 0; i < length; i++) {
+  //   if(device_input[i] == 1) {
+  //     device_output[index] = i;
+  //     index++;
+  //   }
+  // }
   return 0; 
 }
 
