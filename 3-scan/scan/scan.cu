29af8ecf01d7309459c5d9104859a1fffb5eee31
Shahmeer-Khan99 <shahmeerskhan@gmail.com>
2024-11-08 01:55:22 +0100

    Exclusive sum changed 84

--- 3-scan/scan/scan.cu
+++ 3-scan/scan/scan.cu
@@ -342,7 +342,7 @@ int find_repeats(int* device_input, int length, int* device_output) {
 
   // cudaMemcpy(device_input, device, N * sizeof(int), cudaMemcpyHostToDevice);
   // cudaMemcpy(device_result, inarray, N * sizeof(int), cudaMemcpyHostToDevice);
-  int blocks = (rounded_length + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK;
+  int blocks = (length + THREADS_PER_BLOCK - 1)/THREADS_PER_BLOCK;
   map_repeats<<<blocks, THREADS_PER_BLOCK>>>(device_input, length, device_flags);
   cudaDeviceSynchronize();
 
@@ -350,7 +350,7 @@ int find_repeats(int* device_input, int length, int* device_output) {
   exclusive_scan(device_flags, rounded_length, device_scan);
   cudaDeviceSynchronize();
 
-  printDeviceArray(device_scan, length, "DEVICE SCANNN");
+  // printDeviceArray(device_scan, length, "DEVICE SCANNN");
 
   int total_repeats;
   cudaMemcpy(&total_repeats,  device_scan + length - 1, sizeof(int), cudaMemcpyDeviceToHost);
